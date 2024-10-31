c0592f73212a686b3533ff035cbfbc96b20a7784
leerlandais <leerlandais@gmail.com>
2024-10-31 15:24:42 +0100

    first fixtures : OK

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -30,10 +30,35 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
+        $admins = [];
+
         // First off, create Mika and Myself as SUPER
         $super = new User();
         $super->setUsername("leerlandais");
-        $super->setRoles("ROLE_SUPER");
+        $super->setRoles(["ROLE_SUPER"]);
+        $super->setPassword($this->hasher->hashPassword($super, "270675"));
+        $super->setFullname("Lee Brennan");
+        $super->setUniqid(uniqid('user_', true));
+        $super->setEmail("lee@leerlandais.com");
+        $super->setActivate(true);
+
+        $this->admins[] = $super;
+        $manager->persist($super);
+
+        $super = new User();
+        $super->setUsername("mika");
+        $super->setRoles(["ROLE_SUPER"]);
+        $super->setPassword($this->hasher->hashPassword($super, "mjk77"));
+        $super->setFullname("Michael Pitz");
+        $super->setUniqid(uniqid('user_', true));
+        $super->setEmail("michael.pitz@cf2m.be");
+        $super->setActivate(true);
+
+        $this->admins[] = $super;
+        $manager->persist($super);
+
+
+
 
         $manager->flush();
     }
