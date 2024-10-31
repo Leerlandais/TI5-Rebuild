0215443cbab51dcb90170b62be4c26406b45a10f
leerlandais <leerlandais@gmail.com>
2024-10-31 14:59:50 +0100

    started the fixture build

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,8 +5,27 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+// load other dependencies
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory AS Faker;
+use Cocur\Slugify\Slugify;
+use DateTime;
+
+// load my entities
+use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\Section;
+use App\Entity\Tag;
 class AppFixtures extends Fixture
 {
+
+    // construct to set up Faker and Hasher
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+        $this->faker = Faker::create("fr_FR");
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
