86a952f5bb835a20c1e7c4945139cbc19d0893d3
leerlandais <leerlandais@gmail.com>
2024-11-01 09:36:35 +0100

    pagination OK

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,24 +4,29 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'public_home')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(Request $request, PaginatorInterface $paginator, EntityManagerInterface $em): Response
     {
-        $allArticles = $entityManager->getRepository(Article::class)->findAll();
-        $articles = [];
-        foreach ($allArticles as $article) {
-            if ($article->isPublished()) {
-                $articles[] = $article;
-            }
-        }
+        $queryBuilder = $em->getRepository(Article::class)->createQueryBuilder('a')
+            ->where('a.published = 1')
+            ->orderBy('a.id', 'DESC');
+
+        $pagination = $paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page', 1),
+            5
+        );
+
         return $this->render('main/public.main.html.twig', [
-            'articles' => $articles,
+            'pagination' => $pagination,
         ]);
     }
 }
