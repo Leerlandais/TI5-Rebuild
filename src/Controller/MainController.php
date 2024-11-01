508d2a1e24afc27b8d9660869ee54d5277922bea
leerlandais <leerlandais@gmail.com>
2024-11-01 12:30:11 +0100

    moved pagination to a private function

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    private function getAuthors(EntityManagerInterface $em)
+    private function getAuthors(EntityManagerInterface $em) : array
     {
         return $em->getRepository(User::class)->createQueryBuilder('u')
             ->where('u.roles NOT LIKE :role')
@@ -23,40 +23,41 @@ class MainController extends AbstractController
             ->getResult();
     }
 
-    #[Route('/', name: 'public_home')]
-    public function index(Request $request, PaginatorInterface $paginator, EntityManagerInterface $em): Response
+    private function getPagination(EntityManagerInterface $em, PaginatorInterface $paginator, Request $request) : \Knp\Component\Pager\Pagination\PaginationInterface
     {
         $queryBuilder = $em->getRepository(Article::class)->createQueryBuilder('a')
             ->where('a.published = 1')
             ->orderBy('a.id', 'DESC');
 
-        $pagination = $paginator->paginate(
+        return $paginator->paginate(
             $queryBuilder,
             $request->query->getInt('page', 1),
             5
         );
-        $authors = $em->getRepository(User::class)->createQueryBuilder('u')
-            ->where('u.roles NOT LIKE :role')
-            ->setParameter('role', '%ROLE_USER%')
-            ->getQuery()
-            ->getResult();
+    }
+
+    #[Route('/', name: 'public_home')]
+    public function index(EntityManagerInterface $em, PaginatorInterface $pagi, Request $request): Response
+    {
+
 
         $sections = $em->getRepository(Section::class)->findAll();
         return $this->render('main/public.main.html.twig', [
-            'pagination' => $pagination,
+            'pagination' => $this->getPagination($em, $pagi, $request),
             'sections' => $sections,
             'authors' => $this->getAuthors($em),
         ]);
     }
 
     #[Route('/section/{slug}', name: 'public_section')]
-    public function section(EntityManagerInterface $em, $slug): Response
+    public function section(EntityManagerInterface $em, string $slug, PaginatorInterface $pagi, Request $request): Response
     {
         $sections = $em->getRepository(Section::class)->findBy(['section_slug' => $slug]);
 
         return $this->render('main/public.section.html.twig', [
             'sections' => $sections,
             'authors' => $this->getAuthors($em),
+            'pagination' => $this->getPagination($em, $pagi, $request),
         ]);
     }
 }
